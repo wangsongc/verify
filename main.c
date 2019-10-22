f157cac4e9836aa3a9adfb3f1207602cacb9f57f
yuangongji <82787816@qq.com>
2019-10-22 19:07:39 +0800

    Update appveyor.yml

--- main.c
+++ main.c
@@ -5,15 +5,18 @@
 int main()
 {
 	unsigned long a;
+	int r;
+
 	SSL_library_init();
 	ERR_load_crypto_strings();
 	SSL_load_error_strings();
 	OpenSSL_add_all_algorithms();
 
 	a = SSLeay();
+	r = (a == OPENSSL_VERSION_NUMBER);
 	printf("SSLeay = %1x\n", a);
 	printf("OPENSSL_VERSION_NUMBER = %1x\n", OPENSSL_VERSION_NUMBER);
 
-	printf("---end---\n");
-	return !(a == OPENSSL_VERSION_NUMBER); 
+	printf("result--->[%s]\n", r ? "SUCCESS" : "FAIL");
+	return !r; 
 }
